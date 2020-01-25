4d6a1e09fb73e4c74f1744f81e0d754a2dacec51
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-01-25 23:58:38 +0000

    added event listener for burger menu

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("test")
+let burgerMenuButton = document.querySelector("#menuButton");
+let burgerMenu = document.querySelector("#menu");
+
+if (burgerMenuButton) {
+    burgerMenuButton.addEventListener("click",(e) => {
+        burgerMenu.toggle
+    })
+}
