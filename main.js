a0d30b8333e7c11d07ea5f3e92ef8d1cfca58145
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-01-28 15:01:19 +0000

    portofolio change on small screens for 2 top projects working

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 let burgerMenuButton = document.querySelector("#menuButton");
-
 let menu = document.querySelector(".menu");
 
 if (burgerMenuButton) {
     burgerMenuButton.addEventListener("click",(e) => {
-        console.log("OK");
         menu.classList.toggle("no-display")
-        
     })
 }
