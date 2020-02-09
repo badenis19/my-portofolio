829ce0f6f54a0b05faefd2859023baf9b5fcdcd3
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-02-09 22:31:12 +0000

    onscroll menu

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ if (burgerMenuButton) {
     })
 }
 
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+      console.log("AAAA")
+    // document.querySelector(".navbar").style.top = "0";
+    document.querySelector(".navbar").style.background = "none";
+    document.querySelector(".navbar").style.borderBottom = "none";
+    document.querySelector(".links").style.color = "white";
+    
+} else {
+    document.querySelector(".links a").style.color = "black";
+    document.querySelector(".navbar").style.background = "white";
+    // document.querySelector(".navbar").style.borderBottom = "1px solid lightgray";
+    // document.querySelector(".navbar").style.top = "-50px";
+    console.log("BBB")
+  }
+  prevScrollpos = currentScrollPos;
+}
+
