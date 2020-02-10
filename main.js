4b3f50dcd40bb84a2b0de35faf75db45644c9cf3
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-02-10 07:46:40 +0000

    fixed navbar

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
+/* ================= FOR THE BURGER MENU ON SMALLER SCREENS ================= */
+
 let burgerMenuButton = document.querySelector("#menuButton");
 let menu = document.querySelector(".menu");
 
 if (burgerMenuButton) {
-    burgerMenuButton.addEventListener("click",(e) => {
+    burgerMenuButton.addEventListener("click", (e) => {
         menu.classList.toggle("no-display")
     })
 }
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-      console.log("AAAA")
-    // document.querySelector(".navbar").style.top = "0";
-    document.querySelector(".navbar").style.background = "none";
-    document.querySelector(".navbar").style.borderBottom = "none";
-    document.querySelector(".links").style.color = "white";
+/* ================= FOR THE NAVBAR TO APPEAR AND DISSAPEAR ================= */
+
+let links_array = [...document.querySelectorAll(".links a")];
+
+window.onscroll = function () {
     
-} else {
-    document.querySelector(".links a").style.color = "black";
-    document.querySelector(".navbar").style.background = "white";
-    // document.querySelector(".navbar").style.borderBottom = "1px solid lightgray";
-    // document.querySelector(".navbar").style.top = "-50px";
-    console.log("BBB")
-  }
-  prevScrollpos = currentScrollPos;
+    var currentScrollPos = window.pageYOffset;
+
+    if (currentScrollPos < 5) {
+        console.log(currentScrollPos)
+        document.querySelector(".navbar").style.background = "none";
+        document.querySelector(".navbar").style.borderBottom = "none";
+
+        // for all the links to change color
+        links_array.forEach((link) => {
+            link.style.color = "white";
+        })
+
+    } else {
+        console.log(currentScrollPos)
+        links_array.forEach((link) => {
+            link.style.color = "black";
+        })
+
+        document.querySelector(".navbar").style.background = "white";
+    }
+    prevScrollpos = currentScrollPos;
 }
 
